0b4ab94fc75e70293afc3d291994b4e11ab07fa4
joggbe-hash <joggbe@gmail.com>
2026-05-25 21:22:36 +0800

    Deploy Vue app to GitHub Pages

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 import './style.css'
 import LoginPage from './views/LoginPage.vue'
@@ -22,7 +22,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
